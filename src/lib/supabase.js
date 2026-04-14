8264e33364fbc290066fe0c47e8b69264fcfb210
rondaryl03 <Crondaryl@email.com>
2026-04-14 22:27:48 +0800

    feat: initial Vue.js Hello World setup

---
+++ src/lib/supabase.js
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js' 
+const supabaseUrl = 'https://dwqpdfywpvxflklukhsa.supabase.co' 
+const supabaseKey = 'Ysb_publishable_nDuQA1SDj02we7opAVcMsg__fKgb0D4' 
+export const supabase = createClient(supabaseUrl, supabaseKey 
